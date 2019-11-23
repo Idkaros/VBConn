b12d809b85f783b78665d970d0c216e07a062a9c
Manuel Céspedes <virgiliomca@outlook.com>
2019-11-23 12:38:07 -0300

    frmMain

---
+++ VBConn/frmMain.vb
@@ -0,0 +1,21 @@
+﻿Public Class frmMain
+    Dim _conn As Connection
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        _conn = Connection.getInstance
+
+        If _conn.ExistConfigFile = False Then
+            _conn.Generate()
+        Else
+            _conn.Read()
+        End If
+
+        If _conn.ExistConfigFile = False Then
+            lblConexion.Text = "No se pudo realizar la conexión a la base de datos."
+        End If
+
+    End Sub
+
+    Private Sub BtnConexion_Click(sender As Object, e As EventArgs) Handles btnConexion.Click
+        _conn.Generate()
+    End Sub
+End Class
